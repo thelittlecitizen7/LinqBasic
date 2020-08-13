1c372d585621988d704ea9df8993b23ccb21a2ae
thelittlecitizen7 <thelittlecitizen7@gmail.com>
2020-08-13 08:56:14 +0300

    finish linqBasic

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using LinqBasic.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqBasic
+{
+    class Program
+    {
+
+   
+        static void Main(string[] args)
+        {
+            IEnumerable<Person> persons = new List<Person>()
+                                       {
+                                           new Person()
+                                               {
+                                                   Id = 1,
+                                                   Name = "Tomer",
+                                                   PersonGender = Gender.Male
+                                               },
+                                           new Person()
+                                               {
+                                                   Id = 2,
+                                                   Name = "Tami",
+                                                   PersonGender = Gender.Female
+                                               },
+                                                                                              new Person()
+                                               {
+                                                   Id = 3,
+                                                   Name = "Nadav",
+                                                   PersonGender = Gender.Male
+                                               },
+
+        };
+
+            PersonRetriver personRetriver = new PersonRetriver();
+
+            IEnumerable<int> malesIds = personRetriver.GetMalesId(persons);
+
+
+            IEnumerable<Person> peronssOrderByIdDesc = personRetriver.OrderById(persons);
+
+
+            IEnumerable<Person> personsWithOddId = personRetriver.GetOddIds(persons);
+            
+        }
+
+
+
+
+    }
+}
