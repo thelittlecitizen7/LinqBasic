7ef41f06d502f0ff7fedc43e23f3f3d64fa3f8b6
thelittlecitizen <thelittlecitizen7@gmail.com>
2020-08-23 01:35:53 -0700

    fix program and add gitignore

---
+++ PersonRetriver.cs
@@ -0,0 +1,30 @@
+﻿using LinqBasic.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqBasic
+{
+    public class PersonRetriver : IPersonRetriver
+    {
+        public PersonRetriver()
+        {
+            
+        }
+        public IEnumerable<int> GetMalesId(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.PersonGender == Gender.Male).Select(p => p.Id);
+        }
+
+        public IEnumerable<Person> GetOddIds(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.Id % 2 != 0);
+        }
+
+        public IEnumerable<Person> OrderById(IEnumerable<Person> persons)
+        {
+            return persons.OrderByDescending(p => p.Id);
+        }
+    }
+}
